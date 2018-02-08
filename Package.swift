f6eeb087b8a947179ce35d06a4d53631870f4851
Jelle Vandebeeck <jelle@fousa.be>
2018-02-08 08:36:20 +0100

    Add tests to the SPM.

--- Package.swift
+++ Package.swift
@@ -1,8 +1,17 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-  name: "Serializer",
-  dependencies: [
-    .Package(url: "https://github.com/icapps/ios-serializer.git", majorVersion: 1, minor: 0)
-  ]
+    name: "Serializer",
+    products: [
+        .library(name: "Serializer", targets: ["Serializer"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "1.2.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "7.0.3"))
+    ],
+    targets: [
+        .target(name: "Serializer", path: "Sources"),
+        .testTarget(name: "SerializerTests", dependencies: ["Serializer", "Quick", "Nimble"], path: "Tests")
+    ]
 )
---
+++ Tests/JSONSerializerSpecs.swift
@@ -0,0 +1,24 @@
+//
+//  FITMultiSportSpecs.swift
+//  Tests
+//
+//  Created by Jelle Vandebeeck on 13/01/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import Serializer
+
+class JSONSerializerSpecs: QuickSpec {
+    
+    override func spec() {
+        describe("JSONSerializer") {
+            it("should have a correct tests") {
+                expect(true) == true
+            }
+        }
+    }
+    
+}
